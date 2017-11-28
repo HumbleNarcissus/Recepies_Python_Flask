b23c792239147d8b2ba9ecc408a4fbb1187cd93f
humblenarcissus <humblenarcissus@localhost.localdomain>
2017-11-28 23:04:01 +0100

    Update to look like private repo

--- Recepies_Python_Flask/app.py
+++ Recepies_Python_Flask/app.py
@@ -1,29 +1,70 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import Recepies
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, FileField
+from flask_wtf.file import FileRequired
 from passlib.hash import sha256_crypt
 from functools import wraps
+from werkzeug.utils import secure_filename
+import os
+import boto3
+from botocore.client import Config
 
+#Your AWS S3 settings
+'''
+ACCESS_KEY_ID - your account Access Key
+ACCESS_SECRET_KEY - your account Secret Access Key
+BUCKET_NAME - your bucket name where images will be stored
+'''
+ACCESS_KEY_ID = ''
+ACCESS_SECRET_KEY = ''
+BUCKET_NAME = ''
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+)
+
+#Set your secret_key before running
 app = Flask(__name__)
+app.secret_key = ''
 
 #Database config
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '!1Bartosz1!'
-app.config['MYSQL_DB'] = 'users'
+'''
+Set your Database host, user, password and database name
+DictCursor MYSQL_CURSORCLASS is set up correct do not change that
+'''
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MYSQL
 mysql = MySQL(app)
 
+
 @app.route('/')
 def index():
+    cursor = mysql.connection.cursor()
+    #get recepies
+    result = cursor.execute("SELECT * FROM recepies")
+    recepies = cursor.fetchall()
+
+    if result > 0:
+        cursor.close()
+        return render_template('index.html', recepies=recepies)
+    else:
+        cursor.close()
+        return render_template('index.html')
+
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#register form
 class RegisterForm(Form):
     username = StringField('Username', [validators.Length(min=3, max=30)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
@@ -37,6 +78,7 @@ class RegisterForm(Form):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
+
     if request.method == 'POST' and form.validate():
         username = form.username.data
         email = form.email.data
@@ -78,14 +120,18 @@ def login():
                 session['logged_in'] = True
                 session['username'] = username
 
+                #Close connection
+                cursor.close()
                 flash('You are now logged in', 'success')
                 return redirect(url_for('dashboard'))
             else:
+                #Close connection
+                cursor.close()
                 error = "Invalid Login"
                 return render_template('login.html', error=error)
+        else:
             #Close connection
             cursor.close()
-        else:
             error = "Invalid Login"
             return render_template('login.html', error=error)
 
@@ -131,6 +177,14 @@ class RecepieForm(Form):
     ingredients = TextAreaField('Ingredients', [validators.Length(min=10)])
     directions = TextAreaField('Directions', [validators.Length(min=10)])
 
+#allowed file extendsions
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+#checking if extendsion is allowed
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/add_recepie', methods=['GET', 'POST'])
 @is_logged_in
@@ -140,13 +194,30 @@ def add_recepie():
         title = form.title.data
         ingredients = form.ingredients.data
         directions = form.directions.data
+        files = request.files['file']
+
+        #file upload folder
+        target =  'static/images/'
+
+        #check if folder exists
+        if not os.path.isdir(target):
+            os.mkdir(target)
+
+        pic_path = None
+        #check if picture was upload and extendsion allowed
+        if files.filename != '' and allowed_file(files.filename):
+            filename = secure_filename(files.filename)
+            # Image Upload to s3 bucket
+            s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=files, ACL='public-read')
+            pic_path = filename
+
 
         #cursor
         cursor = mysql.connection.cursor()
 
         #execute
-        cursor.execute("INSERT INTO recepies(title, ingredients, directions, author) VALUES(%s, %s, %s, %s)",
-            (title, ingredients, directions, session['username']))
+        cursor.execute("INSERT INTO recepies(title, ingredients, directions, author, picture_path) VALUES(%s, %s, %s, %s, %s)",
+            (title, ingredients, directions, session['username'], pic_path))
 
         #commit to database
         mysql.connection.commit()
@@ -154,7 +225,13 @@ def add_recepie():
         #close connection
         cursor.close()
 
-        flash('Recepie Created', 'success')
+        if files.filename == '':
+            flash('Recepie updated without an image', 'success')
+        elif files.filename != '' and not allowed_file(files.filename):
+            flash('Recepie Updated but image extension is not allowed', 'danger')
+        else:
+            flash('Recepie Updated', 'success')
+
         return redirect(url_for('dashboard'))
     return render_template('add_recepie.html', form=form)
 
@@ -210,17 +287,50 @@ def edit_recepie(id):
     form.ingredients.data = recepie['ingredients']
     form.directions.data = recepie['directions']
 
+    #save img to delate after new submit
+    img_to_erase = recepie['picture_path']
+
     if request.method == 'POST' and form.validate():
         title = request.form['title']
         ingredients = request.form['ingredients']
         directions = request.form['directions']
+        files = request.files['file']
+
+        #file upload to folder
+        target =  'static/images/'
+
+        #check if folder exists
+        if not os.path.isdir(target):
+            os.mkdir(target)
+
+        pic_path = None
+        #check if picture was upload and extension is allowed
+        if files.filename != '' and allowed_file(files.filename):
+            filename = secure_filename(files.filename)
+            # Image Upload
+            s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=files, ACL='public-read')
+            pic_path = filename
+
+
+
+        #delate old img from aws bucket before db commit
+        if img_to_erase != None:
+            s3.Bucket(BUCKET_NAME).delete_objects(
+                Delete = {
+                    'Objects': [
+                        {
+                            'Key': img_to_erase
+                        }
+                    ]
+                }
+            )
 
         # Create Cursor
         cursor = mysql.connection.cursor()
-        app.logger.info(title)
+
         # Execute
-        cursor.execute ("UPDATE recepies SET title=%s, ingredients=%s, directions=%s WHERE id=%s",
-            (title, ingredients, directions, id))
+        cursor.execute ("UPDATE recepies SET title=%s, ingredients=%s, directions=%s, picture_path=%s WHERE id=%s",
+            (title, ingredients, directions, pic_path, id))
 
         # Commit to DB
         mysql.connection.commit()
@@ -228,7 +338,13 @@ def edit_recepie(id):
         #Close connection
         cursor.close()
 
-        flash('Recepie Updated', 'success')
+        #if user didn't upload image
+        if files.filename == '':
+            flash('Recepie updated without an image', 'success')
+        elif files.filename != '' and not allowed_file(files.filename):
+            flash('Recepie Updated but image extension is not allowed', 'danger')
+        else:
+            flash('Recepie Updated', 'success')
 
         return redirect(url_for('dashboard'))
     return render_template('edit_recepie.html', form=form)
@@ -238,6 +354,20 @@ def edit_recepie(id):
 def delete_recepie(id):
     # Create cursor
     cursor = mysql.connection.cursor()
+
+    #Delete image from s3 bucket
+    cursor.execute("SELECT * FROM recepies WHERE id = %s", [id])
+    to_erase = cursor.fetchone()
+    if to_erase['picture_path'] != None:
+        s3.Bucket(BUCKET_NAME).delete_objects(
+            Delete = {
+                'Objects': [
+                    {
+                        'Key': to_erase['picture_path']
+                    }
+                ]
+            }
+        )
 
     # Execute
     cursor.execute("DELETE FROM recepies WHERE id = %s", [id])
@@ -252,6 +382,15 @@ def delete_recepie(id):
 
     return redirect(url_for('dashboard'))
 
+#error handlers for 404 and 500 errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+#Remember to delate debug after all tests
 if __name__ == '__main__':
-    app.secret_key = '<!SecreT!/>'
     app.run(debug=True)
